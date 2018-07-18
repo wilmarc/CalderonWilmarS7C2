47026971577360d00ba7764725ed16b04b5575d7
wilmarc <wa.calderon@uniandes.edu.co>
2018-07-18 12:13:07 -0500

    comment

--- CalderonWilmar_circuitoRC.py
+++ CalderonWilmar_circuitoRC.py
@@ -1,26 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+#Importa los datos ingresados por el txt.
 data=np.genfromtxt("CircuitoRC.txt")
+
+#Define las variables y sus valores del archivo.
 t=data[:,0]
 q=data[:,1]
+
+#Inicializa las variables y constantes.
 Vo=10
 n=10000
 l=np.zeros(n)
 r=np.zeros(n)
 c=np.zeros(n)
 
+
+#Función que define la carga.
 def carga(t,r,c):
 	qmax=Vo*c
 	return qmax*(1-np.exp((-1*t)/(r*c)))
 
+#Funcion de máxima Verosimilitud.
 def like(qes,qdad):
 	suma=0.0
 	for i in range(len(qes)):
 		suma+=(qes[i]-qdad[i])**2
 	suma=suma/n
 	return np.exp((-0.5)*suma)
+
+#Función que define el método de Montecarlo para realizar la estimación Bayesiana.
 def mhm():
 	for i in range(n):
 		rguess=20.0*np.random.random()
@@ -46,7 +55,11 @@ def mhm():
 				r[i]=rguess
 				l[i]=lprimero
 				c[i]=cguess
+
+#Llama la optimización de MonteCarlo.
 mhm()		
+
+#Define los valores máximos con r y c optimizado y Qmax calculado.
 indice=np.argmax(l)
 rbest=r[indice]
 cbest=c[indice]
@@ -56,6 +69,7 @@ cd2="Mejor C: "+str(round(cbest,5))
 cd3="Qmax: "+ str(round(qmax,5))
 print (rbest,cbest,qmax)
 
+#Genera La gráfica con la regresión hecha.
 plt.figure()
 plt.scatter(t,q,s=2)
 plt.plot(t,carga(t,rbest,cbest),c="g")
@@ -66,8 +80,8 @@ plt.ylabel("$q(t)$")
 plt.xlabel("$t$")
 plt.tight_layout() 
 plt.savefig("CargaRC.pdf")
-#plt.show()
 
+#Genera las gráficas r Vs. -ln(l) y c Vs. -ln(l)
 plt.figure()
 plt.subplot(211)
 plt.scatter(r,-np.log(l),s=1)
@@ -79,5 +93,4 @@ plt.xlabel("r")
 plt.ylabel("$-ln(l)$")
 plt.tight_layout()
 plt.savefig("OPCIONAL.pdf")
-plt.show()
 
